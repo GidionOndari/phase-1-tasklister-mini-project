7c6ee9d9dce230a6bfb8f98e944084c3f18867d0
ondarigidion <gidion.ondari@student.moringaschool.com>
2025-07-16 10:18:48 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// your code here
+  const form = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); // Prevent page reload
+
+    const newTask = taskInput.value.trim();
+
+    if (newTask !== "") {
+      const li = document.createElement("li");
+      li.textContent = newTask;
+
+      //  Add delete button
+      const delBtn = document.createElement("button");
+      delBtn.textContent = "❌";
+      delBtn.addEventListener("click", () => li.remove());
+// Clear input
+      li.appendChild(delBtn);
+      taskList.appendChild(li);
+      taskInput.value = ""; 
+    }
+  });
 });
+
